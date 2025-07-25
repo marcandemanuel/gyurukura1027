b8cbd47d204b3b3282ead4e090ead24df6ba37a0
marcandemanuel <marcandemanuel@gmail.com>
2025-07-25 09:48:41 +0200

    Commit

--- services/config_service.py
+++ services/config_service.py
@@ -1,12 +1,13 @@
 import json
+import os
 
 class ConfigService:
     def __init__(self):
         self.config = self.load_config()
     
     def load_config(self) -> dict:
-        import os
-        config_path = os.environ.get("CONFIG_PATH", "config.json")
+        config_path = os.environ.get("CONFIG_PATH")
+        print(config_path)
         with open(config_path, 'r', encoding='utf-8') as f:
             return json.load(f)
         
