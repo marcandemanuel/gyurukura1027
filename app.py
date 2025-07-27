5d08e9c3641fdcbc900ec1c38173f823083880c0
marcandemanuel <marcandemanuel@gmail.com>
2025-07-27 18:15:23 +0200

    ae

--- app.py
+++ app.py
@@ -494,5 +494,5 @@ if __name__ == "__main__":
 
     is_send_emails(config_service.get_config()['emails'])
     
-    port = int(os.environ.get("PORT", 2020))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
