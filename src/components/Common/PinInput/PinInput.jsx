d68662f2b215cea84d83a21cda9148420f4d4d20
marcandemanuel <marcandemanuel@gmail.com>
2025-08-02 15:05:29 +0200

    cf

--- src/components/Common/PinInput/PinInput.jsx
+++ src/components/Common/PinInput/PinInput.jsx
@@ -174,7 +174,7 @@ const PinInput = ({
                     className={styles.hiddenPasswordInput}
                     tabIndex={-1}
                     aria-hidden="true"
-                    autoComplete="off"
+                    autoComplete="one-time-code"
                 />
 
                 <h2 className={styles.title}>{title}</h2>
